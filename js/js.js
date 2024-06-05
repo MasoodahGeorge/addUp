003c8de5c94049c19ade291ce2eaf7acde3e528a
MasoodahGeorge <oodasbusiness@gmail.com>
2024-06-05 09:31:23 +0200

    .

--- js/js.js
+++ js/js.js
@@ -1,4 +1,6 @@
-function addUp(arr) {
-    return arr.reduce((total, num) => total + num, 0);
-}
-console.log(addUp([1, 2, 3]));
+document.querySelector('.button').addEventListener('click', function() {
+    let input = document.getElementById('input').value;
+    let numbers = input.split(',').map(Number);
+    let sum = numbers.reduce((acc, curr) => acc + curr, 0);
+    document.querySelector('.answer').innerText = sum;
+});
